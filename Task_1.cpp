36cbf8ce6c03b7b7bf565f8c547b1f9ab91da68a
MrZorg111 <yodji333@mail.ru>
2023-03-07 13:40:31 +0300

    Final test close

--- Task_1.cpp
+++ Task_1.cpp
@@ -1,58 +1,71 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <ctime>
 #include <iomanip>
+#include <vector>
 
+struct data {
+    std::string name = " ";
+    int begin_job = 0;
+    int end_job = 0;
+};
 
 int main() {
-    std::string command, job = " ";
-    std::time_t t;
-    std::map<std::string, int> start;
-    std::map<std::string, int> finish;
+    std::vector<data> data_job;
+    std::string command, job;
+    int i = -1;
+
     do {
-        std::cout << "Enter the command: ";
+        std::cout << "Enter the command (begin/end/status/exit):";
         std::cin >> command;
         if (command == "begin") {
-            if (job == " ") {
+            i++;
+            data_job.resize(i);
+            if (data_job.empty()) {
+                data_job.push_back(data());
+                std::cout << "Enter the name of the task:";
+                std::cin >> job;
+                std::time_t t = std::time(nullptr);
+                data_job[i].name = job;
+                data_job[i].begin_job = t;
+            }
+            else {
+                data_job.push_back(data());
+                if (data_job[i - 1].end_job == 0) {
+                    std::time_t t_end = std::time(nullptr);
+                    data_job[i - 1].end_job = t_end;
+                }
+                std::cout << "Enter the name of the task:";
+                std::cin >> job;
+                std::time_t t_start = std::time(nullptr);
+                data_job[i].name = job;
+                data_job[i].begin_job = t_start;
+            }
+        }
+        else if (command == "end" ) {
+           std::time_t t_end = std::time(nullptr);
+           data_job[i].end_job = t_end;
+        }
+        else if (command == "status" ) {
+            if (data_job.empty()) {
+                std::cout << "There are no completed and ongoing tasks!\n";
+            }
+            else {
+                for (int l = 0; l < data_job.size(); l++) {
+                    if (data_job[l].end_job != 0) {
+                        std::time_t stat = std::difftime(data_job[l].end_job, data_job[l].begin_job);
+                        std::tm local = *std::localtime(&stat);
+                        std::cout << "To complete the task " << data_job[l].name << " spent "
+                                  << std::put_time(&local, "%M:%S") << "\n";
+                    }
+                    else {
+                        std::cout << "Current task:" << data_job[l].name << "\n";
+                    }
 
-                std::cout << "Enter the name of the task:";
-                std::cin >> job;
-                t = std::time(nullptr);
-                start.insert(std::pair<std::string, int> (job, t));
+                }
             }
-            else {
-                std::time_t tf = std::time(nullptr);
-                finish.insert(std::pair<std::string, int> (job, (int)difftime(tf, t)));
-                std::cout << "Enter the name of the task:";
-                std::cin >> job;
-                std::time_t ts = std::time(nullptr);
-                start.insert(std::pair<std::string, int> (job, ts));
-            }
-        }
-        else if (command == "end") {
-            if (job != " ") {
-                std::time_t tfin = std::time(nullptr);
-                finish.insert(std::pair<std::string, int> (job, (int)difftime(tfin, t)));
-                job = " ";
-            }
-        }
-        else if (command == "status") {
-            if (job != " ") {
-                std::cout << "Task in execution: " << job << "\n";
-            }
-            else {
-                std::cout << "There are no completed tasks!\n";
-            }
-            for (std::map<std::string, int> :: iterator fin_j = finish.begin(); fin_j != finish.end(); fin_j++) {
-                t = fin_j -> second;
-                std::tm local = *localtime(&t);
-                std::cout << "To complete the task " << fin_j -> first << " it was spent " << std::put_time(&local, "%M:%S") << "\n";
-            }
-        }
-        else if (command != "exit") {
-            std::cout << "Invalid code\n";
         }
     }while (command != "exit");
+
     return 0;
 }
